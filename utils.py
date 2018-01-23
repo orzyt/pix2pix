3c78aa8ee4a3b58597a649c8ebec4d1a3118ffdd
orzyt <xmzyt1996@gmail.com>
2018-01-23 12:37:02 +0800

    update: some utility functions

--- utils.py
+++ utils.py
@@ -1,14 +1,37 @@
+import numpy as np
+from PIL import Image
+import imageio
 from datetime import datetime
 
+SCALE = 286
+CROP = 256
+
+
 def log(file, str, end="\n"):
-    print("%s: %s" % (datetime.now(), str), end=end)
-    file.write("%s: %s%s" % (datetime.now(), str, end))
+    print("%s: %s" % (datetime.now().strftime('%Y/%m/%d %X'), str), end=end)
+    file.write("%s: %s%s" % (datetime.now().strftime('%Y/%m/%d %X'), str, end))
     file.flush()
 
-def preprocess(img):
-    # [0, 1] => [-1, 1]
-    return img * 2 - 1
 
-def deprocess(img):
-    # [-1, 1] => [0, 1]
-    return (img + 1) / 2
+def preprocess(image):
+    return image / 127.5 - 1
+
+
+def transfrom(image, off_w, off_h):
+    image = image.resize((SCALE, SCALE), Image.BILINEAR)
+    image = image.crop((off_w, off_h, off_w + CROP, off_h + CROP)).getdata()
+    image = np.array(image).reshape((CROP, CROP, 3))
+    image = preprocess(image)
+    return image
+
+
+def get_image(file_name):
+    image = Image.open(file_name)
+    width, height = image.size
+    offset = np.random.randint(0, SCALE - CROP + 1, size=(2,))
+    image_a = transfrom(image.crop((0, 0, width // 2, height)), offset[0], offset[1])
+    image_b = transfrom(image.crop((width // 2, 0, width, height)), offset[0], offset[1])
+    return [image_a, image_b]
+
+def save_images(image, file_name):
+    return imageio.imwrite(file_name, image)
