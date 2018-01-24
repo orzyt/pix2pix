1ed57d6811d20ef4b18a05e8aa47f10d6e9e0fe0
orzyt <xmzyt1996@gmail.com>
2018-01-24 14:04:54 +0800

    update: code formatting

--- ops.py
+++ ops.py
@@ -5,4 +5,5 @@ def lrelu(x, leak=0.2, name='leaky_relu'):
     with tf.variable_scope(name):
         f1 = 0.5 * (1 + leak)
         f2 = 0.5 * (1 - leak)
-        return f1 * x + f2 * tf.abs(x)
+
+    return f1 * x + f2 * tf.abs(x)
